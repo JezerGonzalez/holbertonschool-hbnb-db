ecdc7d445e67f3d3233a9b80b89debb37179ded6
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-26 16:17:47 -0400

    added query for database

--- src/persistence/db.py
+++ src/persistence/db.py
@@ -14,30 +14,55 @@
 
 from src.models.base import Base
 from src.persistence.repository import Repository
+from flask import current_app as app
 
 
 class DBRepository(Repository):
     """Dummy DB repository"""
 
     def __init__(self) -> None:
-        """Not implemented"""
+        """Sets the configuration as the app"""
+        self.useData = app.config['USE_DATABASE']
 
     def get_all(self, model_name: str) -> list:
-        """Not implemented"""
-        return []
+        """Gets all models in the database"""
+        if self.useData:
+            return app.db.session.query(model_name).all()
+        else:
+            return []
 
     def get(self, model_name: str, obj_id: str) -> Base | None:
-        """Not implemented"""
+        """Returns data for the given model"""
+        if self.useData:
+            return app.db.session.query(model_name).filter_by(id=obj_id).first()
+        else:
+            return None
 
     def reload(self) -> None:
         """Not implemented"""
 
     def save(self, obj: Base) -> None:
-        """Not implemented"""
+        """Saves the given model"""
+        if self.useData:
+            app.db.session.add(obj)
+            app.db.session.commit()
+        else:
+            pass
 
     def update(self, obj: Base) -> Base | None:
-        """Not implemented"""
+        """Updates the given model"""
+        if self.useData:
+            app.db.session.add(obj)
+            app.db.session.commit()
+            return obj
+        else:
+            return None
 
     def delete(self, obj: Base) -> bool:
-        """Not implemented"""
-        return False
+        """Deletes the given model"""
+        if self.useData:
+            app.db.session.delete(obj)
+            app.db.session.commit()
+            return True
+        else:
+            return False
